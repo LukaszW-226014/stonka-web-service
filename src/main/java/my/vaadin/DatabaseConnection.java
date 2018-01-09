26a946a32a710facb4853bffc6ee5d0991b97462
Unknown <226014@student.pwr.edu.pl>
2018-01-09 16:21:52 +0100

    added database connection

---
+++ src/main/java/my/vaadin/DatabaseConnection.java
@@ -0,0 +1,45 @@
+package my.vaadin;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/stonkav1";
+
+    //  Database credentials
+    static final String USER = "stonka";
+    static final String PASS = "Administrator";
+
+    public DatabaseConnection() {
+        connect();
+    }
+
+    public void connect() {
+        Connection conn = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+    }
+}
