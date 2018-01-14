6f34a214e28d13465c1e0f4a311ca8094f62f02b
Unknown <226014@student.pwr.edu.pl>
2018-01-14 16:50:22 +0100

    add data Class to connect Database ro KlientPanel

--- src/main/java/my/vaadin/Ankiety.java
+++ src/main/java/my/vaadin/Ankiety.java
@@ -1,4 +1,45 @@
 package my.vaadin;
 
-public class Ankiety {
+import java.io.Serializable;
+
+public class Ankiety implements Serializable {
+    private int idAnkiety;
+    private String tytul;
+    private  String opis;
+
+    public Ankiety(int idAnkiety, String tytul, String opis) {
+        this.idAnkiety = idAnkiety;
+        this.tytul = tytul;
+        this.opis = opis;
+    }
+
+
+    public int getIdAnkiety() {
+        return idAnkiety;
+    }
+
+    public void setIdAnkiety(int idAnkiety) {
+        this.idAnkiety = idAnkiety;
+    }
+
+    public String getTytul() {
+        return tytul;
+    }
+
+    public void setTytul(String tytul) {
+        this.tytul = tytul;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    @Override
+    public String toString() {
+        return idAnkiety + " " + tytul + " " + opis;
+    }
 }
--- src/main/java/my/vaadin/Pytanie.java
+++ src/main/java/my/vaadin/Pytanie.java
@@ -1,4 +1,34 @@
 package my.vaadin;
 
-public class Pytanie {
+import java.io.Serializable;
+
+public class Pytanie implements Serializable {
+    private int idPytania;
+    private String tresc;
+
+    public Pytanie(int idPytania, String tresc) {
+        this.idPytania = idPytania;
+        this.tresc = tresc;
+    }
+
+    public int getIdPytania() {
+        return idPytania;
+    }
+
+    public void setIdPytania(int idPytania) {
+        this.idPytania = idPytania;
+    }
+
+    public String getTresc() {
+        return tresc;
+    }
+
+    public void setTresc(String tresc) {
+        this.tresc = tresc;
+    }
+
+    @Override
+    public String toString() {
+        return idPytania + ". " + tresc;
+    }
 }
